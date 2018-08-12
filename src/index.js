3d3d5a1f566c16ab7737732aa7a1f84566045eb3
Dillon Jones <dillon.z.jones@gmail.com>
2018-08-11 23:55:21 -0400

    fixup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './2048.css';
+import './2D.css';
 import './3D.css';
 import Game from './components/game';
 import registerServiceWorker from './registerServiceWorker';
